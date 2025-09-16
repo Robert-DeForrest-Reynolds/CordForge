fff0fc4409e2132dd54bf5b2d6b485a668767dd3
robert-deforrest-reynolds <rldr.europa@gmail.com>
2025-09-16 12:55:51 -0700

    Updated examples

--- Examples/example/example.py
+++ Examples/example/example.py
@@ -1,20 +1,20 @@
 from CordForge import *
 
+roc = Cord("roc")
+
 
 # Initial send of dashboard, all other functions are replys/edits of the sent message
 async def entry(user_card:Card) -> Card:
     await user_card.new_image()
     panel = await user_card.panel(border=True)
     await user_card.text("Hello", Vector2(5, 5), parent=panel)
-    await user_card.add_button("Scavenge", scavenge_report, [])
+    await user_card.add_button("Lorem", lorem, [])
 
 
-async def scavenge_report(user_card:Card, interaction) -> None:
+async def lorem(user_card:Card, interaction) -> None:
     await user_card.new_image()
     await user_card.add_button("Home", roc.home, [])
     await roc.reply(user_card, interaction)
 
 
-roc = Cord("roc", entry)
-# any necessary setup
-roc.launch()
+roc.launch(entry)
--- Examples/traits.py
+++ Examples/traits.py
@@ -1,6 +1,14 @@
 from CordForge import *
 
 
+bot = Cord(entry_command="cmd")
+
+bot.user_traits = [
+    # [trait name, trait value]
+    ["wallet", 0.00],
+]
+
+
 # Initial send of dashboard, all other functions are replys/edits of the sent message
 async def entry(user_card:Card) -> Card:
     await user_card.add_button(f"Money: {user_card.user.wallet}", give_money, [])
@@ -11,11 +19,4 @@ async def give_money(user_card:Card, interaction) -> None:
     await bot.home(user_card, interaction)
 
 
-player_traits = [
-    # [trait name, trait value]
-    ["wallet", 0.00],
-]
-bot = Cord(entry_command="cmd",
-           entry=entry,
-           player_traits=player_traits)
-bot.launch()
+bot.launch(entry)
